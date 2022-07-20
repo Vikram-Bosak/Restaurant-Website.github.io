41d25531629b61326b274f82bd7404152c383582
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-07-21 01:57:02 +0530

    i have chaged the file name

--- note.js
+++ note.js
@@ -7,3 +7,9 @@
 // => 4 git status   to check the status  or git status -s
 
 // => 5 git add -A    to add files to Github
+
+// => 6 git add -A    to add files to Github
+
+// => 7 git add file name  to add files to Github
+
+// => 8 git commit -m " keep learning "   to commit a file or a final commit 
