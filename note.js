30746762fd69586326980a2e3d49604a9e02dd07
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-07-21 01:53:58 +0530

    now i'm learning a git cammand

---
+++ note.js
@@ -0,0 +1,9 @@
+// =>1. git config --global user.name
+
+// => 2. git config --global user.email
+
+// => 3. ls  or ls -la - to show all files
+
+// => 4 git status   to check the status  or git status -s
+
+// => 5 git add -A    to add files to Github
